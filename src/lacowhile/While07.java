a10905a1be559f795b44728b9ea25bdd81ea7d8a
MatusMoura2 <matus.moura333x2@gmail.com>
2023-10-01 17:21:20 -0300

    while07

---
+++ src/lacowhile/While07.java
@@ -0,0 +1,14 @@
+package lacowhile;
+
+public class While07 {
+
+	public static void main(String[] args) {
+		int num = 1;
+
+		while (num <= 100) {
+			System.out.print(num + " ");
+			num++;
+		}
+	}
+
+}
