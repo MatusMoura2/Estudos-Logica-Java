d5994703d1f1176d67002faddbe2dbfa10db2284
MatusMoura2 <matus.moura333x2@gmail.com>
2023-10-02 23:07:30 -0300

    while09

---
+++ src/lacowhile/While09.java
@@ -0,0 +1,29 @@
+/*Escreva um programa Java, para encontrar 
+ * a soma de todos os números entre 1 e N.*/
+package lacowhile;
+
+import java.util.Scanner;
+
+public class While09 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		int soma = 0;
+		int loop = 1;
+		System.out
+				.print("Esse programa faz a soma de um até um número, em qual numero você quer terminar o contador? ");
+		num = sc.nextInt();
+
+		if (num > 1) {
+			while (loop <= num) {
+				soma += loop;
+				loop++;
+			}
+			System.out.printf("A soma dos números entre 1 e %d é %d",num,soma);
+		} else {
+			System.out.println("O numero tem que ser maior do que 1");
+		}
+	}
+
+}
