3d160de0558d29ce68919c391645b8d44498dd7c
MatusMoura2 <matus.moura333x2@gmail.com>
2023-10-07 08:34:39 -0300

    while15

---
+++ src/lacowhile/While15.java
@@ -0,0 +1,30 @@
+/*Escreva um programa Java, para reverter
+os dígitos fornecidos como entrada.*/
+package lacowhile;
+
+import java.util.Scanner;
+
+public class While15 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String digitos;
+		String resultado = "";
+		char digitos2;
+
+		System.out.print("Digite digitos para serem revertidos: ");
+		digitos = sc.nextLine();
+
+		int i = digitos.length() - 1;
+		while (i >= 0) {
+			digitos2 = digitos.charAt(i);
+			resultado += digitos2;
+			i--;
+		}
+		
+		System.out.printf("%s ao contrario é %s.",digitos,resultado);
+
+	}
+
+}
